f87ce657c9056df19c63b495c76d151e5a63d598
rameezhub <iamrameezsarguru@gmail.com>
2026-02-06 01:05:28 +0530

    Fix swagger api-docs route

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -1,12 +1,20 @@
-const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerJSDoc = require("swagger-jsdoc");
 
-module.exports = swaggerJsDoc({
+const options = {
   definition: {
     openapi: "3.0.0",
     info: {
       title: "Gov Scheme API",
       version: "1.0.0",
+      description: "Government Scheme Recommendation Backend",
     },
+    servers: [
+      {
+        url: "https://gov-scheme-backend-1.onrender.com",
+      },
+    ],
   },
   apis: ["./src/routes/*.js"],
-});
+};
+
+module.exports = swaggerJSDoc(options);
