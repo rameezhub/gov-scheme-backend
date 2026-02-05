ba5e624b532eff2836c37f2cef9b547532ef79ea
rameezhub <iamrameezsarguru@gmail.com>
2026-02-06 00:47:13 +0530

    Switch database to PostgreSQL for Render

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,15 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: "mysql",
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
   }
-);
+});
 
 module.exports = sequelize;
