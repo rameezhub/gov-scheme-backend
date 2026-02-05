467a2239b9628433db6d0265326f1ac34ccbe232
rameezhub <iamrameezsarguru@gmail.com>
2026-02-06 03:34:29 +0530

    Backend stable: auth, schemes, eligibility, JWT

--- src/controllers/scheme.controller.js
+++ src/controllers/scheme.controller.js
@@ -11,9 +11,20 @@ exports.getAllSchemes = async (req, res) => {
 };
 
 // GET recommended schemes (temporary simple logic)
-exports.getRecommendations = async (req, res) => {
+
+exports.recommendSchemes = async (req, res) => {
+  const { age, income, state } = req.query;
+
   try {
-    const schemes = await Scheme.findAll();
+    const schemes = await Scheme.findAll({
+      where: {
+        minAge: { [Op.lte]: age },
+        maxAge: { [Op.gte]: age },
+        maxIncome: { [Op.gte]: income },
+        states: { [Op.like]: `%${state}%` }
+      }
+    });
+
     res.json(schemes);
   } catch (err) {
     res.status(500).json({ message: err.message });
